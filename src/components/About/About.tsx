8bc672d9d31fe2df1a927031fd39fbd48d0735cb
Valentin Schegolev <shchegolev.dev@gmail.com>
2026-09-24 16:50:27 +0300

    chore: add correct about section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,11 +1,15 @@
 import { Section } from '../Section/Section';
-import { ABOUT_TEXT } from '../../data/resumeData';
+import { ABOUT_PARAGRAPHS } from '../../data/resumeData';
 import styles from './About.module.css';
 
 export const About = () => {
   return (
     <Section id="about" title="Обо мне">
-      <p className={styles.about__text}>{ABOUT_TEXT}</p>
+      {ABOUT_PARAGRAPHS.map((paragraph) => (
+        <p key={paragraph} className={styles.about__text}>
+          {paragraph}
+        </p>
+      ))}
     </Section>
   );
 };
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,19 +1,28 @@
-import { Section } from '../Section/Section';
-import { PROJECTS } from '../../data/resumeData';
-import { cx } from '../../utils/cx';
-import styles from './Projects.module.css';
+import { Section } from "../Section/Section";
+import { PROJECTS } from "../../data/resumeData";
+import { cx } from "../../utils/cx";
+import styles from "./Projects.module.css";
 
 export const Projects = () => {
+  if (PROJECTS.length === 0) {
+    return null;
+  }
+
   return (
     <Section id="projects" title="Пет-проекты">
       <div className={styles.projects__grid}>
         {PROJECTS.map((project) => (
           <div
             key={project.id}
-            className={cx(styles.projects__card, project.isPlaceholder && styles['projects__card--placeholder'])}
+            className={cx(
+              styles.projects__card,
+              project.isPlaceholder && styles["projects__card--placeholder"],
+            )}
           >
             <h3 className={styles.projects__title}>{project.title}</h3>
-            <p className={styles.projects__description}>{project.description}</p>
+            <p className={styles.projects__description}>
+              {project.description}
+            </p>
           </div>
         ))}
       </div>
